2efc7bcb4478c96ed4416ef58178f7ab9123b501
Vladislav Klepikov <ya.losyash@yandex.ru>
2024-03-20 19:53:04 +0300

    replace ArrayList on HashMap in Game.java

--- src/main/java/TournamentClasses/Game.java
+++ src/main/java/TournamentClasses/Game.java
@@ -3,36 +3,30 @@ package TournamentClasses;
 import DataClasses.Player;
 import Exceptions.NotRegisteredException;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
-    private ArrayList<Player> players = new ArrayList<>();
+    private HashMap<String, Integer> map = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        map.put(player.getName(), player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        int strengthPlayer1 = findPlayer(playerName1).getStrength();
-        int strengthPlayer2 = findPlayer(playerName2).getStrength();
-        if (strengthPlayer1 > strengthPlayer2) {
+        int strengthP1 = findPlayer(playerName1);
+        int strengthP2 = findPlayer(playerName2);
+        if (strengthP1 > strengthP2) {
             return 1;
-        } else if (strengthPlayer1 < strengthPlayer2) {
+        } else if (strengthP1 < strengthP2) {
             return 2;
         }
         return 0;
     }
 
-//    private void isRegistered(String playerName) {
-//        if (!players.contains(playerName)) {
-//            throw new NotRegisteredException(playerName);
-//        }
-//    }
-
-    private Player findPlayer(String playerName) {
-        for (Player player : players) {
-            if (player.getName().equals(playerName)) {
-                return player;
+    private int findPlayer(String playerName) {
+        for (String player : map.keySet()) {
+            if (player.equals(playerName)) {
+                return map.get(player);
             }
         }
         throw new NotRegisteredException(playerName);
